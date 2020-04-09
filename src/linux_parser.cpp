16ad9abe04ef5b9ecead3bcb77745d383a78ad74
tasmanx <m.kuncevicius@gmail.com>
2020-04-09 07:19:44 +0000

    Implement OperatingSystem function

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -10,7 +10,6 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// DONE: An example of how to read data from the filesystem
 string LinuxParser::OperatingSystem() {
   string line;
   string key;
