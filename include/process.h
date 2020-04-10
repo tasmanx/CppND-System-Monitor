043c04f64f99c8ca6d4af0371766ef1e5c78cda4
tasmanx <m.kuncevicius@gmail.com>
2020-04-10 15:31:56 +0000

    Implement Pid function

--- include/process.h
+++ include/process.h
@@ -9,7 +9,7 @@ It contains relevant attributes as shown below
 class Process {
  public:
   Process(int pid) : pid_(pid) {}
-  int Pid();                               // TODO: See src/process.cpp
+  int Pid();                               
   std::string User();                      // TODO: See src/process.cpp
   std::string Command();                   // TODO: See src/process.cpp
   float CpuUtilization();                  // TODO: See src/process.cpp
@@ -17,7 +17,6 @@ class Process {
   long int UpTime();                       // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
 
-  // TODO: Declare any necessary private members
  private:
    int pid_ = {}; 
 };
