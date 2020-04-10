e7a2a4764ae11ee87f5b96b3c28a460421025bd0
tasmanx <m.kuncevicius@gmail.com>
2020-04-10 13:10:49 +0000

    Implement Utilization function

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,17 @@
-#include "processor.h"
+#include <vector>
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+#include "processor.h"
+#include "linux_parser.h"
+
+using std::vector;
+
+// Return the aggregate CPU utilization
+float Processor::Utilization() {
+  vector<int> cpu_time = LinuxParser::CpuUtilization();
+  int total_time = 0;
+  for (int time : cpu_time)
+    total_time += time;
+  float time_being_in_idle = cpu_time[3] / (float)total_time;
+  float time_not_in_idle = 1.0 - time_being_in_idle;
+  return time_not_in_idle;
+}
