f1148a4b39ecf86afc2492791d88b32ff9f7e737
tasmanx <m.kuncevicius@gmail.com>
2020-04-11 20:04:20 +0000

    Delete unnecessary code

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -110,19 +110,6 @@ long LinuxParser::UpTime() {
   return value;
 }
 
-// TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
-
-// TODO: Read and return the number of active jiffies for a PID
-// REMOVE: [[maybe_unused]] once you define the function
-long LinuxParser::ActiveJiffies(int pid[[maybe_unused]]) { return 0; }
-
-// TODO: Read and return the number of active jiffies for the system
-long LinuxParser::ActiveJiffies() { return 0; }
-
-// TODO: Read and return the number of idle jiffies for the system
-long LinuxParser::IdleJiffies() { return 0; }
-
 vector<int> LinuxParser::CpuUtilization() {
   vector<int> cpu_time;
   int value;
