8cb16520960e679da4f519c3ec1c169b89a889bf
tasmanx <m.kuncevicius@gmail.com>
2020-04-11 08:46:44 +0000

    Implement Command function

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -170,9 +170,15 @@ int LinuxParser::RunningProcesses() {
   return value;
 }
 
-// TODO: Read and return the command associated with a process
-// REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Command(int pid) { 
+  string line; 
+  std::ifstream filestream(kProcDirectory + std::to_string(pid) + kCmdlineFilename);
+  if (filestream.is_open()) {
+    std::getline(filestream, line);
+    return line;
+  }
+  return line;
+}
 
 // Return memory utilization in KB
 int LinuxParser::Ram(int pid) {
